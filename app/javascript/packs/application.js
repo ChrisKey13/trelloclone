40578cc5f3b66f0f9c946d26cda7215f177edca3
christophe bouquet <christophe.productowner@gmail.com>
2020-03-22 23:46:49 +0100

    vue implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,23 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("turbolinks").start()
 require("channels")
 
 import "bootstrap";
+
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+document.addEventListener("turbolinks:load", function() {
+  var element = document.querySelector("#boards")
+  if (element != undefined) {
+    const app = new Vue ({
+        el: element,
+        data: {
+          lists: JSON.parse(element.dataset.lists)
+        },
+        template: "<App :original_lists='lists' />",
+        components: { App }
+      })
+  }
+});
